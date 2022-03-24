e35e42f37d51f379a9213aa1e0df590cb3f64fa3
techiejd <62455039+techiejd@users.noreply.github.com>
2022-03-24 16:11:22 -0500

    Refactors transactions.

--- App.js
+++ App.js
@@ -1,78 +1,18 @@
-import React, { useState, useEffect } from "react";
-import { Image, Text, View, StyleSheet, Picker, Pressable } from "react-native";
-import { BarCodeScanner } from "expo-barcode-scanner";
-import {Camera} from 'expo-camera';
+import React from 'react';
+import { SafeAreaView, Platform, StatusBar } from 'react-native';
 
-export default function App() {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [scanned, setScanned] = useState(false);
-  const [sending, setSending] = useState(false);
+import Transactions from './components/transactions/transactions';
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === "granted");
-    })();
-  }, []);
-
-  const handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-  };
-
-  if (hasPermission === null) {
-    return <Text>Requesting for camera permission</Text>;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
-
-  return (
-    <View style={styles.container}>
-      {/* <Picker style={styles.picker} itemStyle={styles.pickerItem}>
-              <Picker.Item label="Poblado | $30K" value="poblado" />
-              <Picker.Item label="ComMonSys | $33K" value="commonsys" />
-      </Picker> */}
-      <Pressable
-        style={{
-          width: 200,
-          height: 200,
-          flex: 1,
-          alignItems: "center",
-        }}
-        onPress={() => {
-          setSending(!sending);
-        }}
-      >
-        {sending ? (
-            <View style={{width:260,height:250}}>
-            <Camera onBarCodeScanned={handleBarCodeScanned} style={{flex:1}}/>
-            </View>
-      ) : (
-        <Image
-          source={require("./assets/qrCode.png")}
-          style={{
-            width: 200,
-            height: 200,
-          }}
-        />
-      )}
-      </Pressable>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+export default function App(){
+  return(<>
+  <SafeAreaView style={
+  {
     flexDirection: "column",
-    justifyContent: "space-between",
+    justifyContent: "space-around",
     alignItems: "center",
-  },
-  picker: {
-    width: 250,
-  },
-  pickerItem: {
-    color: "black",
-  },
-});
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  }}>
+    <Transactions/>
+  </SafeAreaView>
+  </>)
+}
---
+++ components/transactions/transactions.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from "react";
+import { Image, Text, View, StyleSheet, Picker, Pressable } from "react-native";
+import {Camera} from 'expo-camera';
+
+export default function Transactions() {
+
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [sending, setSending] = useState(true);
+
+  const toggleSending = () => {
+    setSending(!sending);
+}
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View>
+      <Picker style={styles.picker} itemStyle={styles.pickerItem}>
+              <Picker.Item label="Poblado | $30K" value="poblado" />
+              <Picker.Item label="ComMonSys | $33K" value="commonsys" />
+      </Picker>
+      <View
+        style={styles.qrLike}
+      >
+        {sending ? (
+                <Camera onBarCodeScanned={handleBarCodeScanned} style={styles.full} autoFocus={'on'}>
+                    <Pressable
+                        style={styles.full}
+                        onPress={toggleSending}
+                    />
+                </Camera>
+        ) : (
+            <Pressable
+                style={styles.full}
+                onPress={toggleSending}
+            a>
+                <Image
+                    source={require("../../assets/qrCode.png")}
+                    style={styles.full}
+                />
+            </Pressable>
+        )
+        }
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  picker: {
+    width: 250,
+  },
+  pickerItem: {
+    color: "black",
+  },
+  qrLike: {
+      width: 200,
+      height: 200,
+  },
+  full: {
+      width: "100%",
+      height: "100%",
+  },
+});
