33ae641d5c5d2b5cb6324fef0c02f669f20502de
techiejd <62455039+techiejd@users.noreply.github.com>
2022-03-25 13:31:07 -0500

    Does some formatting and removes some unused imports.

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
-import React, {Component} from 'react';
-import { SafeAreaView, Platform, StatusBar } from 'react-native';
+import React, { Component } from 'react'
+import { SafeAreaView, Platform, StatusBar } from 'react-native'
 
-import Transactions from './components/Transactions';
-import ShopsMap from './components/Shops';
+import Transactions from './components/Transactions'
+import ShopsMap from './components/Shops'
 
-export default class App extends Component{
-  render() {
+export default class App extends Component {
+  render () {
     /* <ShopsMap/>*/
-    return(<>
-      <SafeAreaView style={
-      {
-        flexDirection: "column",
-        justifyContent: "space-around",
-        alignItems: "center",
-        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
-      }}>
-        <Transactions/>
-        <ShopsMap/>
-      </SafeAreaView>
-      </>)
+    return (
+      <>
+        <SafeAreaView
+          style={{
+            flexDirection: 'column',
+            justifyContent: 'space-around',
+            alignItems: 'center',
+            paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+          }}
+        >
+          <Transactions />
+          <ShopsMap />
+          <Timeline />
+        </SafeAreaView>
+      </>
+    )
   }
 }
--- components/Shops.js
+++ components/Shops.js
@@ -1,23 +1,14 @@
-import React, {Component} from 'react';
-import { WebView } from 'react-native-webview';
-import { View } from "react-native";
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import React from 'react'
+import { WebView } from 'react-native-webview'
+import { View } from 'react-native'
 
-class ShopsMapWebView extends Component {
-    render() {
-        return( <WebView source={{ uri: 'https://reactnative.dev/' }} />);
-      }
-}
-
-export default function ShopsMaps() {
-    return(<>
-        <View
-            style={{ width: 400, height:400}}
-        >
-            <WebView
-                style={{ width: "100%", height:"100%"}}
-                source={{
-                    html: `
+export default function ShopsMaps () {
+  return (
+    <View style={{ width: 400, height: 400 }}>
+      <WebView
+        style={{ width: '100%', height: '100%' }}
+        source={{
+          html: `
                     <!DOCTYPE html>
                     <html>
                         <head><meta name="viewport" content="width=device-width, initial-scale=1"></head>
@@ -25,9 +16,9 @@ export default function ShopsMaps() {
                             <iframe width="100%" height="100%" src="https://www.google.com/maps/d/embed?mid=1Gx28fiAtmIKgeVZo5GkTysyuBf_logAd&ehbc=2E312F" frameborder="0"></iframe>
                         </div></body>
                     </html>
-                    `,
-                    }}/>
-
-        </View>
-    </>)
+                    `
+        }}
+      />
+    </View>
+  )
 }
