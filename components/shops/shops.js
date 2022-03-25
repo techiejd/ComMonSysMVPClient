1acf5f71ad16b6e7e7c33b907408a290025b16e3
techiejd <62455039+techiejd@users.noreply.github.com>
2022-03-25 13:08:17 -0500

    Has Maps MVP.

--- components/shops/shops.js
+++ components/shops/shops.js
@@ -2,26 +2,6 @@ import React, {Component} from 'react';
 import { WebView } from 'react-native-webview';
 import { View } from "react-native";
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-function ShopsMapPrev() {
-    return(
-        // <MapView
-        //         style={{ width:100, height:100 }}
-        //         provider={PROVIDER_GOOGLE}
-        //         initialRegion={{
-        //         latitude: 37.78825,
-        //         longitude: -122.4324,
-        //         latitudeDelta: 0.0922,
-        //         longitudeDelta: 0.0421}}
-        // ></MapView>
-        <WebView
-            source={{
-                uri: `
-                <iframe width="100%" height="100%" src="http://www.weather.gov/" frameborder="0"></iframe>
-                `,
-            }}
-            />
-    )
-}
 
 class ShopsMapWebView extends Component {
     render() {
@@ -34,7 +14,19 @@ export default function ShopsMaps() {
         <View
             style={{ width: 400, height:400}}
         >
-            <WebView source={{html: '<iframe width="100%" height="100%" src="https://www.google.com/maps/d/embed?mid=1Gx28fiAtmIKgeVZo5GkTysyuBf_logAd&ehbc=2E312F" frameborder="0"></iframe>'}} />
+            <WebView
+                style={{ width: "100%", height:"100%"}}
+                source={{
+                    html: `
+                    <!DOCTYPE html>
+                    <html>
+                        <head><meta name="viewport" content="width=device-width, initial-scale=1"></head>
+                        <body><div style="height:100vh;">
+                            <iframe width="100%" height="100%" src="https://www.google.com/maps/d/embed?mid=1Gx28fiAtmIKgeVZo5GkTysyuBf_logAd&ehbc=2E312F" frameborder="0"></iframe>
+                        </div></body>
+                    </html>
+                    `,
+                    }}/>
 
         </View>
     </>)
