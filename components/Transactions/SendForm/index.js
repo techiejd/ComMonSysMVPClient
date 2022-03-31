3e64a90ec6167ec0e39508b3d56433940a49d04e
techiejd <62455039+techiejd@users.noreply.github.com>
2022-03-31 13:43:42 -0500

    Formats.

--- components/Transactions/SendForm/index.js
+++ components/Transactions/SendForm/index.js
@@ -1,32 +1,25 @@
-import React, { useState, useEffect } from "react";
-import {
-  Text,
-  View,
-  StyleSheet,
-  Pressable,
-  Modal,
-  TextInput,
-} from "react-native";
-import SendMoneyForm from "./SendMoneyForm";
-import SendVoteForm from "./SendVoteForm";
-import ErrorForm from "./ErrorForm";
+import React, { useState, useEffect } from 'react'
+import { View, StyleSheet, Modal } from 'react-native'
+import SendMoneyForm from './SendMoneyForm'
+import SendVoteForm from './SendVoteForm'
+import ErrorForm from './ErrorForm'
 
-function isInvalid(data) {
-  return true;
+function isInvalid (data) {
+  return false
 }
-function isVoting(data) {
-  return true;
+function isVoting (data) {
+  return false
 }
 
-export default function SendForm({ visible, setVisible, data }) {
+export default function SendForm ({ visible, setVisible, data }) {
   return (
     <Modal
-      animationType="slide"
+      animationType='slide'
       transparent={true}
       visible={visible}
       onRequestClose={() => {
-        Alert.alert("Modal has been closed.");
-        setVisible(!visible);
+        Alert.alert('Modal has been closed.')
+        setVisible(!visible)
       }}
     >
       <View style={styles.centeredView}>
@@ -41,32 +34,32 @@ export default function SendForm({ visible, setVisible, data }) {
         </View>
       </View>
     </Modal>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   centeredView: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22
   },
   modalView: {
-    backgroundColor: "white",
+    backgroundColor: 'white',
     borderRadius: 20,
     padding: 15,
-    alignItems: "center",
-    shadowColor: "#000",
+    alignItems: 'center',
+    shadowColor: '#000',
     shadowOffset: {
       width: 0,
-      height: 2,
+      height: 2
     },
     shadowOpacity: 0.25,
     shadowRadius: 4,
     elevation: 5,
     height: 200,
-    flexDirection: "column",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-});
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  }
+})
