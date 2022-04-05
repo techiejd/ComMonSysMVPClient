aa26580b18b32532968b5ae03d8860642630cf2f
techiejd <62455039+techiejd@users.noreply.github.com>
2022-04-04 22:33:33 -0500

    Adds blockchain context provider. Fixes silly balances bug

--- components/Transactions/hooks.js
+++ components/Transactions/hooks.js
@@ -1,46 +1,45 @@
-import React, { useState, useEffect } from "react";
-import "@ethersproject/shims";
-import { ethers } from "ethers";
-import ERC20ABI from "../../constants/ERC20ABI";
+import React, { useState, useEffect, useContext } from 'react'
+import '@ethersproject/shims'
+import { ethers } from 'ethers'
+import ERC20ABI from '../../constants/ERC20ABI'
+import { CommunityCoinAddress } from '../../constants/Contracts'
+import { BlockchainContext } from '../BlockchainProvider'
 
-// TODO(techiejd): Create and save the users' address.
-const userAddress = "0xCca2bd5957073026b56Cdaaeb282AD4a61619a3a"; // JD's public ComMonSys MVP Ethereum address
-// TODO(techijd): Maybe move this out of here and to the constants/ file.
-const communityCoinAddress = "0x9a1a38d91A0844E76A0e8262b0965c83536b7892";
+// Returns balances as { coms: 'x,xxx', pc: 'x,xxx' }
+const useBalances = (coin /** coms || pc */) => {
+  const { userAddress, provider } = useContext(BlockchainContext)
+  const [coinBalances, setCoinBalances] = useState({ coms: null, pc: null })
 
-// Returns balance as string as 'x,xxx'
-const useBalance = (coin /** coms || pc */) => {
-  const [balance, setBalance] = useState(null);
-  const provider = new ethers.providers.JsonRpcProvider({
-    url: "https://137.184.238.79/rpc",
-    timeout: 15000,
-  });
-  const prettify = (balance) => {
-    return ethers.utils.commify(Math.floor(ethers.utils.formatEther(balance)));
-  };
+  const prettify = balance => {
+    return ethers.utils.commify(Math.floor(ethers.utils.formatEther(balance)))
+  }
+
+  const communityCoinContract = new ethers.Contract(
+    CommunityCoinAddress,
+    ERC20ABI,
+    provider
+  )
+
   useEffect(() => {
-    (async () => {
-      switch (coin) {
-        case "coms":
-          provider
-            .getBalance(userAddress)
-            .then((balance) => setBalance(prettify(balance)))
-            .catch((error) => alert(error));
-        case "pc":
-          communityCoinContract = new ethers.Contract(
-            communityCoinAddress,
-            ERC20ABI,
-            provider
-          );
+    ;(async () => {
+      provider
+        .getBalance(userAddress)
+        .then(comsBalance => {
           communityCoinContract.callStatic
             .balanceOf(userAddress)
-            .then((balance) => setBalance(prettify(balance)))
-            .catch((error) => alert(error));
-      }
-    })();
-  }, []);
+            .then(pcBalance =>
+              setCoinBalances({
+                coms: prettify(comsBalance),
+                pc: prettify(pcBalance)
+              })
+            )
+            .catch(error => alert(error))
+        })
+        .catch(error => alert(error))
+    })()
+  }, [])
 
-  return balance;
-};
+  return coinBalances
+}
 
-export { useBalance };
+export { useBalances }
