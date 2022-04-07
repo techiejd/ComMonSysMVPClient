58dd3f3a293bebdc2cef6fca87a6d99f14bef01e
jimenez1917 <jimenez1917@gmail.com>
2022-04-07 18:09:11 -0500

    VoteStoreProvider WIP

---
+++ providers/VoteStoreProvider.js
@@ -0,0 +1,22 @@
+import React, { useContext, useState, useEffect } from "react";
+
+const VoteStoreContext = React.createContext();
+
+const VoteStoreProvider = ({children})=>{
+    const [voteMessage,setVoteMessage]=useState('');
+    const [voteFlag,setvoteFlag]=useState(false);
+
+    const voting = ()=>{
+        setvoteFlag(true);
+    }
+
+    const message="Muchas gracias por tu voto, juntos creamos comunidad";
+    if(voteFlag)
+    setTimeout(setVoteMessage(message), 5000);
+
+    return (
+        <VoteStoreContext.Provider value={{ voting, voteMessage }}>
+          {children}
+        </VoteStoreContext.Provider>
+      );
+}
